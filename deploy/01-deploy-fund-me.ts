a31d80a960cd4386db6eb4a9afb9610456f4ea99
Suhel-Kap <suhelkapadia2@gmail.com>
2022-07-14 18:28:22 +0530

    Code refactoring

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -1,40 +1,42 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
-import { networkConfig, developmentChains } from "../helper-hardhat-config"
 import { verify } from "../utils/verify"
+import { networkConfig, developmentChains } from "../helper-hardhat-config"
 
-const deployFund: DeployFunction = async function ({
-    getNamedAccounts,
-    deployments,
-    network,
-}) {
-    // extracted from HRE
-    const { deploy, log, get } = deployments
+const deployFundMe: DeployFunction = async function (
+    hre: HardhatRuntimeEnvironment
+) {
+    // @ts-ignore
+    const { getNamedAccounts, deployments, network } = hre
+    const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId: number = network.config.chainId!
 
-    // const ethUsdPriceFeedAddress: string = networkConfig[chainId]
-    //     .ethUsdPriceFeed!;
-
-    let ethUsdPriceFeedAddress
-    if (developmentChains.includes(network.name)) {
-        const ethUsdAggregator = await get("MockV3Aggregator")
+    let ethUsdPriceFeedAddress: string
+    if (chainId == 31337) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
         ethUsdPriceFeedAddress = networkConfig[network.name].ethUsdPriceFeed!
     }
+    log("----------------------------------------------------")
+    log("Deploying FundMe and waiting for confirmations...")
 
     const fundMe = await deploy("FundMe", {
         from: deployer,
         args: [ethUsdPriceFeedAddress],
         log: true,
-        waitConfirmations: networkConfig[network.name].blockConfirmations! || 1,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations:
+            (networkConfig[network.name].blockConfirmations as number) || 0,
     })
-    log("**********************************************")
-
-    if (!developmentChains.includes(network.name)) {
-        verify(fundMe.address, [ethUsdPriceFeedAddress])
+    log(`FundMe deployed at ${fundMe.address}`)
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
     }
 }
-export default deployFund
-deployFund.tags = ["all", "fundme"]
+export default deployFundMe
+deployFundMe.tags = ["all", "fundMe"]
